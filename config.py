e797e275d69eaef035990b806942403df686f7b6
BADIAMOHAMEDAYMANE <badiamohamedaymane@gmail.com>
2025-08-04 16:38:40 +0100

    Create a refresh token

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +10,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/parc_informatique'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', SECRET_KEY)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
--- src/controllers/authentification_controller.py
+++ src/controllers/authentification_controller.py
@@ -1,36 +1,51 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import (
+    get_jwt_identity,
+    get_jwt,
+    create_access_token,
+    create_refresh_token,
+    jwt_required
+)
 from src.services.authentification_service import AuthentificationService
 
 authentication_bp = Blueprint('auth', __name__)
 
+
+@authentication_bp.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    try:
+        result = AuthentificationService.inscrire_utilisateur(data)
+        return jsonify(result), 201
+    except ValueError as e:
+        return jsonify({"msg": str(e)}), 400
+    except Exception as e:
+        return jsonify({"msg": f"Erreur lors de la création : {str(e)}"}), 500
+
+
 @authentication_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
-
     try:
-        access_token = AuthentificationService.login_utilisateur(data)
-        return jsonify(access_token=access_token), 200
-
+        result = AuthentificationService.login_utilisateur(data)
+        return jsonify(result), 200
     except ValueError as e:
         return jsonify({"msg": str(e)}), 401
     except Exception as e:
         return jsonify({"msg": f"Erreur serveur : {str(e)}"}), 500
 
 
+@authentication_bp.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh_token():
+    identity = get_jwt_identity()
+    claims = get_jwt()
 
-
-
-@authentication_bp.route('/signup', methods=['POST'])
-def signup():
-    data = request.json
-
-    try:
-        result = AuthentificationService.inscrire_utilisateur(data)
-        return jsonify(result), 201
-
-    except ValueError as e:
-        return jsonify({"msg": str(e)}), 400
-
-    except Exception as e:
-        return jsonify({"msg": f"Erreur lors de la création : {str(e)}"}), 500
-
+    new_access_token = create_access_token(
+        identity=identity,
+        additional_claims={
+            "role": claims.get("role"),
+            "email": claims.get("email")
+        }
+    )
+    return jsonify(access_token=new_access_token), 200
