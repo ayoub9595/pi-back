96d98c6975d449f253bc4676d7101954945ba535
Ayoub Badiâ <ayoub.badia95@gmail.com>
2025-08-30 21:01:41 +0100

    Change reclamation mail template

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/parc_informatique'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/parc_informatique'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', SECRET_KEY)
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
