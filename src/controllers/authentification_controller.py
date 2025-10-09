545cf7594eff501880091260a49684ad168d24a7
abadia <abadia@norsys.fr>
2025-10-09 09:23:18 +0100

    Add change password method in authentication controller

--- src/controllers/authentification_controller.py
+++ src/controllers/authentification_controller.py
@@ -3,7 +3,6 @@ from flask_jwt_extended import (
     get_jwt_identity,
     get_jwt,
     create_access_token,
-    create_refresh_token,
     jwt_required
 )
 from src.services.authentification_service import AuthentificationService
@@ -49,3 +48,26 @@ def refresh_token():
         }
     )
     return jsonify(access_token=new_access_token), 200
+
+@authentication_bp.route('/change-password', methods=['PUT'])
+@jwt_required()
+def change_password():
+    current_user_id = get_jwt_identity()
+
+    data = request.json
+
+    if not data.get('ancien_mot_de_passe') or not data.get('nouveau_mot_de_passe'):
+        return jsonify({"msg": "Ancien et nouveau mot de passe requis"}), 400
+
+    try:
+        AuthentificationService.changer_mot_de_passe(
+            id_utilisateur=int(current_user_id),
+            ancien_mot_de_passe=data['ancien_mot_de_passe'],
+            nouveau_mot_de_passe=data['nouveau_mot_de_passe']
+        )
+
+        return jsonify({"msg": "Mot de passe changé avec succès"}), 200
+    except ValueError as e:
+        return jsonify({"msg": str(e)}), 400
+    except Exception as e:
+        return jsonify({"msg": f"Erreur serveur : {str(e)}"}), 500
